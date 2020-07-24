ad5893f9ac774be26f3ec82f3da0c5cf52b9d118
Kota <jatak307@gmail.com>
2020-07-24 14:30:47 +0300

    module ShowInfo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/form';
+import ShowInfo from './modules/showInfo';
 
 window.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new MainSlider({btns: '.next', container: '.page'});
@@ -43,4 +44,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   new Difference('.officerold', '.officernew', '.officer__card-item').init();
   new Form('.form').init();
+
+  new ShowInfo('.plus__content').init();
 });
