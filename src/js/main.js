b953a8a7a2281ab51d44d8b9d126cfe6e96dc20e
Kota <jatak307@gmail.com>
2020-07-24 18:41:43 +0300

    module Download

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/form';
 import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new MainSlider({btns: '.next', container: '.page'});
@@ -46,4 +47,6 @@ window.addEventListener('DOMContentLoaded', () => {
   new Form('.form').init();
 
   new ShowInfo('.plus__content').init();
+
+  new Download('.download').init();
 });
